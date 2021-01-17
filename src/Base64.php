6109530da6b8e20de1c40945ddbf1242ae840d58
Andre Polykanine <ap@oire.me>
2021-01-17 15:07:04 +0100

    Add Psalm support

--- src/Base64.php
+++ src/Base64.php
@@ -37,10 +37,10 @@ class Base64
 
     /**
      * Encoding into URL-safe Base64.
-     * @param  mixed  $data The data to be encoded
+     * @param  string  $data The data to be encoded
      * @return string The encoded data or an empty string on failure
      */
-    public static function encode($data, bool $replaceEquals = false): string
+    public static function encode(string $data, bool $replaceEquals = false): string
     {
         $b64 = base64_encode($data);
 
